b88514d183943dac0a04856add08919ebc47f032
Henrique114 <h.ferreira114@gmail.com>
2025-01-17 23:19:21 -0300

    criado camada de teste

--- Purchase.Test/obj/Debug/net8.0/Purchase.Test.AssemblyInfo.cs
+++ Purchase.Test/obj/Debug/net8.0/Purchase.Test.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Purchase")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Purchase.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da0c90224e922eff20c339c03b2e6512c92a4d83")]
-[assembly: System.Reflection.AssemblyProductAttribute("Purchase")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Purchase")]
+[assembly: System.Reflection.AssemblyProductAttribute("Purchase.Test")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Purchase.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
